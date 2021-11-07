5dd6449469c9880ae9b6144e9380ec9ec8cbe562
vita_sokolova <viktoriia.sokolova@ah.nl>
2021-11-07 14:10:39 +0300

    Add ui tests

---
+++ app/src/androidTest/java/com/example/testingsampleapp/presentation/overview/WeatherOverviewActivityTest.kt
@@ -0,0 +1,89 @@
+package com.example.testingsampleapp.presentation.overview
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.testingsampleapp.R
+import com.example.testingsampleapp.data.ForecastRepository
+import com.example.testingsampleapp.domain.models.DailyForecast
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import io.mockk.coEvery
+import kotlinx.coroutines.delay
+import org.hamcrest.Matchers.not
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.time.LocalDateTime
+import javax.inject.Inject
+
+@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
+class WeatherOverviewActivityTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var forecastRepository: ForecastRepository
+
+    private val dailyForecast = DailyForecast(
+        currentTemperature = 40.0,
+        dayTemperature = 42.0,
+        nightTemperature = 48.2,
+        created = LocalDateTime.now()
+    )
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun whenDataIsLoadedShouldShowTemperatureTexts() {
+        coEvery { forecastRepository.getDailyForecast(any()) } returns dailyForecast
+        ActivityScenario.launch(WeatherOverviewActivity::class.java).use {
+            onView(withId(R.id.loading_indicator))
+                .check(matches(not(isDisplayed())))
+            onView(withId(R.id.data_container))
+                .check(matches(isDisplayed()))
+            onView(withId(R.id.error_container))
+                .check(matches(not(isDisplayed())))
+        }
+    }
+
+    @Test
+    fun whenLoadingFailsShouldShowErrorState() {
+        coEvery { forecastRepository.getDailyForecast(any()) } throws Throwable()
+        ActivityScenario.launch(WeatherOverviewActivity::class.java).use {
+            onView(withId(R.id.loading_indicator))
+                .check(matches(not(isDisplayed())))
+            onView(withId(R.id.data_container))
+                .check(matches(not(isDisplayed())))
+            onView(withId(R.id.error_container))
+                .check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun whenRetryButtonIsClickedLoadingStarts() {
+        coEvery { forecastRepository.getDailyForecast(any()) } throws Throwable()
+        ActivityScenario.launch(WeatherOverviewActivity::class.java).use {
+
+            onView(withId(R.id.retry_btn))
+                .perform(click())
+
+            coEvery { forecastRepository.getDailyForecast(any()) } coAnswers {
+                delay(1000L)
+                dailyForecast
+            }
+
+            onView(withId(R.id.loading_indicator))
+                .check(matches(not(isDisplayed())))
+        }
+    }
+}
---
+++ app/src/androidTest/java/com/example/testingsampleapp/presentation/overview/di/TestForecastModule.kt
@@ -0,0 +1,38 @@
+package com.example.testingsampleapp.presentation.overview.di
+
+import com.example.testingsampleapp.data.*
+import com.example.testingsampleapp.utils.di.DispatchersModule
+import com.example.testingsampleapp.utils.di.ForecastModule
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import io.mockk.mockk
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [ForecastModule::class]
+)
+class TestForecastModule {
+
+    private val mockApi: ForecastApi by lazy { mockk(relaxed = true) }
+    private val mockForecastDataSource: ForecastDataSource by lazy { mockk(relaxed = true) }
+    private val mockForecastRepository: ForecastRepository by lazy { mockk(relaxed = true) }
+
+    @Provides
+    @Singleton
+    fun provideForecastApi(): ForecastApi = mockApi
+
+    @Provides
+    @Singleton
+    fun provideForecastDataSource(): ForecastDataSource = mockForecastDataSource
+
+    @Provides
+    @Singleton
+    fun provideForecastRepository(): ForecastRepository = mockForecastRepository
+}
